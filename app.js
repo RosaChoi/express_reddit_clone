2b7aff635129f725a3e9f005a98b2261274aeae1
Zubair Desai <zubairdesai@Zubairs-MacBook-Pro-2.local>
2015-06-17 15:42:35 -0700

    Add loginmiddelware, routemiddleware, bcrypt

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var express = require('express'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
     db = require('./models');
+    loginMiddleware = require("./middleware/loginHelper");
+    routeMiddleware = require("./middleware/routeHelper");
 
 app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  maxAge: 3600000,
+  secret: 'supersecretro',
+  name: "double chocochip"
+}));
 
 //ROOT
 app.get('/', function(req,res) {
@@ -67,7 +75,6 @@ app.delete('/posts/:id', function(req,res){
       post.remove()
       res.redirect('/posts')
     }
-
   })
 })
 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
@@ -20,6 +22,53 @@ var userSchema = new mongoose.Schema ({
                         ref: "Comment"
                       }]
                     });
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  return bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    return bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+userSchema.statics.authenticate = function (formData, callback) {
+  this.findOne({
+      email: formData.email
+    },
+    function (err, user) {
+      console.log(user)
+      if (user === null){
+        callback("Invalid username or password",null);
+      }
+      else {
+        user.checkPassword(formData.password, callback);
+      }
+
+    });
+};
+
+userSchema.methods.checkPassword = function(password, callback) {
+  var user = this;
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (isMatch) {
+      callback(null, user);
+    } else {
+      callback(err, null);
+    }
+  });
+};
+
 
 var User = mongoose.model("User", userSchema);
 module.exports = User;
